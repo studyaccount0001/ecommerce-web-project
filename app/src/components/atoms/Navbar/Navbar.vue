c2b37187bd0aca12c62ff5eb554d915ba200ae0a
Paulo <paulosoares.other@gmail.com>
2022-05-03 15:43:10 -0300

    More changes

---
+++ app/src/components/atoms/Navbar/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <nav>
+            <router-link to="/">Home</router-link>
+            <router-link to="/foods">Food</router-link>
+            <router-link to="/treats">Treats</router-link>
+            <router-link to="/supplies">Supplies</router-link>
+        </nav>
+        <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+};
+</script>
+
+<style lang="scss" scoped>
+nav {
+    @apply bg-fluffBlue-12 p-7 text-fluffBlue-2 flex gap-4;
+
+    a {
+        @apply font-normal text-fluffBlue-1;
+
+        &.router-link-exact-active {
+            @apply font-bold text-fluffBlue-5;
+        }
+    }
+}
+</style>
---
+++ app/src/views/FoodView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="main">
+        <h1>This is Food page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FoodView",
+};
+</script>
+
+<style lang="scss" scoped>
+.main {
+    @apply bg-fluffBlue-4;
+}
+</style>
